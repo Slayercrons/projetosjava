2b2ca7a154639c0be1b10fb9e45af56d13df522a
Natanael de Aquino <70642225+Slayercrons@users.noreply.github.com>
2021-05-07 09:21:09 -0300

    Add files via upload

---
+++ projeto/src/projeto/Projeto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+
+public class Projeto {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario calc = new Formulario();
+        calc.setVisible(true);
+    }
+    
+}
